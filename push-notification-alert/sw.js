9d320d5d8ffdd91fbd1cb53394d54f6c5013d570
Pranay Goel <goel.2002pranay@gmail.com>
2023-09-14 19:23:18 +0530

    fix: service worker

--- push-notification-alert/sw.js
+++ push-notification-alert/sw.js
@@ -1,9 +1,15 @@
 console.log('Service Worker Loaded...');
 
-self.addEventListener('push', (e) => {
+this.addEventListener('activate', function (e) {
+    console.log('Service Worker Activated...');
+});
+
+self.addEventListener('push', async function (e) {
     const data = e.data.json();
-    console.log('Push Received...');
-    self.registration.showNotification(data.title, {
+    console.log('Push Received...', data);
+    console.log(self);
+
+    this.registration.showNotification(data.title, {
         body: data.body
     })
     // const notification = new self.Notification(data.title, {
