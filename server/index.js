ee4f369c451d3fa0ef1e9c9ae8596c0a04819a51
Pranay Goel <goel.2002pranay@gmail.com>
2023-09-14 17:28:13 +0530

    fixes

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.send("Welcome to Push Notification Server");
 });
 
