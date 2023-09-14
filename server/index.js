dd47b289e53c516d456602dd7df2aa2279bec1c3
Pranay Goel <goel.2002pranay@gmail.com>
2023-09-14 17:35:48 +0530

    remove cors for server

--- server/index.js
+++ server/index.js
@@ -2,23 +2,9 @@ const express = require("express");
 const webpush = require("web-push");
 const notificationRoute = require("./routes/notification.routes");
 const dbConnect = require("./config/dbconnect");
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-  })
-);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
